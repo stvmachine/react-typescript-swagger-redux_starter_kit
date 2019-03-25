bac2db3ccdea1a90ff9b621b61f8286fe78aa5c1
stvmachine <campos.esteban@gmail.com>
2019-03-26 00:08:49 +1100

    Removed old implementation

---
+++ src/store/pets/index.tsx
@@ -0,0 +1,51 @@
+import { createSlice, createSelector } from 'redux-starter-kit'
+import { Pet } from './types'
+
+export interface IStoreState {
+  readonly pets: ReadonlyArray<Pet>
+  readonly errorMessage?: String
+  readonly isFetching: boolean
+}
+
+export interface IAddPetPayload {
+  name: string
+  photoUrls: Array<string>
+}
+
+const INITIAL_STATE: IStoreState = {
+  pets: new Array(),
+  isFetching: false,
+  errorMessage: '',
+}
+
+const pets = createSlice({
+  slice: 'pets',
+  initialState: INITIAL_STATE,
+  reducers: {
+    get: state => {
+      state.errorMessage = ''
+      state.isFetching = true
+    },
+    getSuccess: (state, { payload }) => {
+      state.isFetching = false
+      state.pets = payload
+    },
+    getFailure: (state, { payload }) => {
+      state.isFetching = false
+      state.errorMessage = payload
+    },
+    add: state => {
+      state.errorMessage = ''
+    },
+    addSuccess: (state, { payload }) => {
+      state.pets = [...state.pets, payload]
+    },
+    addFailure: (state, { payload }) => {
+      state.errorMessage = payload
+    },
+  },
+})
+
+pets.selectors.getPets = createSelector(['pets.pets'])
+
+export default pets
